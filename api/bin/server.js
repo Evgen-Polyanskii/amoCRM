713275e250e15e54a608ae9ba3368a29f6e6e995
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-05-27 19:09:05 +0300

    Deploy heroku

--- api/bin/server.js
+++ api/bin/server.js
@@ -1,16 +1,23 @@
 #! /usr/bin/env node
 
 const getApp = require('../index.js');
+const db = require('../../db/models');
 
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 4000;
 
-const app = getApp();
-
-app.listen(app.get('port'), HOST, (err) => {
-  if (err) {
-    console.log(`App crashed ${err}`);
+db.sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+    getApp().listen(PORT, HOST, (err) => {
+      if (err) {
+        console.log(`App crashed ${err}`);
+        process.exit(1);
+      }
+      console.log(`Server was started on '${PORT}'`);
+    });
+  })
+  .catch(() => {
+    console.log('Connection to the database is not established.');
     process.exit(1);
-  }
-  console.log(`Server was started on '${PORT}'`);
-});
+  });
