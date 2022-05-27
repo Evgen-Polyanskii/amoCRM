913d6cfc42fc9b70d4d7aa3c820debd1b53a29d8
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-05-27 16:58:32 +0300

    Deploy heroku

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -4,6 +4,13 @@ const createDeal = require('../services/createDeal.js');
 
 module.exports = (app) => {
   app
+    .get('/', (req, res, next) => {
+      try {
+        res.status(200).send('Welcome!');
+      } catch (err) {
+        next(err);
+      }
+    })
     .get('/contacts', async (req, res, next) => {
       try {
         const errors = validate(req.query);
